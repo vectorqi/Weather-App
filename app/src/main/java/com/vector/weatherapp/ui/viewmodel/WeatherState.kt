f114391013e5a92f3157458feaec95377879742c
Vector <qeemiaomiao@gmail.com>
2025-02-06 15:10:20 -0800

    Update WeatherState.kt

--- app/src/main/java/com/vector/weatherapp/ui/viewmodel/WeatherState.kt
+++ app/src/main/java/com/vector/weatherapp/ui/viewmodel/WeatherState.kt
@@ -1,5 +1,5 @@
 package com.vector.weatherapp.ui.viewmodel
-
+//View state 
 sealed class WeatherState {
     object Loading : WeatherState() // 表示加载状态
     data class Success(val data: com.vector.weatherapp.data.models.WeatherResponse) : WeatherState() // 表示成功状态
